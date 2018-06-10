1758950ad26534a6c8ec78619158d25c0dd564b5
Lawyer <lawyerh_01@yahoo.com>
2018-06-09 19:53:49 -0700

    api is making contact with sql

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bp = require('body-parser');
 const mysql = require('mysql');
 const cors = require("cors");
+const bcrypt = require('bcrypt');
+const credentials = require('./mysqlconnection');
 
 //***** CONFIG ********/
 
@@ -11,6 +13,16 @@ const app = express();
 
 app.use(bp.json());
 app.use(cors());
+
+const saltRounds = 10;
+
+const con = mysql.createConnection({
+    host: credentials.host,
+    user: credentials.user,
+    password: credentials.password,
+    port: credentials.port,
+    database: credentials.database
+});
 
 //***** ROUTES ********/
 
@@ -21,7 +33,39 @@ app.get('/', (req, res) => {
 app.post('/login', (req,res) => {
     const values = req.body
     console.log(values);
-    res.json("Hello")
+    con.query(`SELECT * FROM users where(emailAddress = '${values.email}')`, (err, user) => {
+        
+        if(err) console.log(err);
+        
+        if(user) {
+            bcrypt.compare(values.password, user.password, (err, result) => {
+                if(err) console.log(`***************password didnt match: ${err}`)
+            })
+        };
+    })
+})
+
+app.post('/newUser', (req,res) => {
+
+    const sub = req.body;
+    console.log(sub);
+
+    bcrypt.hash(sub.password, saltRounds, (err, hash) => {
+
+        if(err){
+            res.json({error: err});
+        }
+        // else {
+        //     console.log(hash);
+        // }
+        // else {
+        //     con.query(`INSERT INTO users (firstName, lastName, birthdate, emailAddress, gender, userName, password, created_at, updated_at) VALUES ('${sub.fName}', '${sub.lName}', '${sub.birthday}', '${sub.email}', '${sub.gender}', '${sub.userName}', '${hash}', now(), now());`, function(err, response){
+        //         if (err) console.log(err);
+        //         if (response) console.log(response);
+        //     })
+        // }
+
+    })
 })
 
 
